3a7876a8437559d0c9e25ad49fdf2499aea0f0e8
eckff <44920739+eckff@users.noreply.github.com>
2020-08-24 23:03:13 +0300

    Исправляем последствия порта

--- update_start.sh
+++ update_start.sh
@@ -2,6 +2,6 @@ killall -s 9 DreamDaemon
 
 git pull
 
-sudo DreamMaker baystation12.dme
+sudo DreamMaker nebula.dme
 
-sudo nohup nice -n -20 DreamDaemon baystation12.dmb 7777 -trusted -logself -public -threads on -map-threads on &
+sudo nohup nice -n -20 DreamDaemon nebula.dmb 7777 -trusted -logself -public -threads on -map-threads on &
